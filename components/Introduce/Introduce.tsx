110136dbc3d389e248a71a0d2b8fe6b015e8fcbc
Piotr <piotr.szczypka@vp.pl>
2022-11-03 18:57:36 +0100

    introduce section

--- components/Introduce/Introduce.tsx
+++ components/Introduce/Introduce.tsx
@@ -3,11 +3,15 @@ import Image from 'next/image';
 
 export default function Introduce() {
   return (
-    <div className={'section-container relative min-h-screen overflow-hidden '}>
-      <h3 className={'section-indicator pt-4'}>Introduce</h3>
+    <div className={'section-container relative min-h-screen overflow-hidden'}>
       <h2 className={'section-title'}>Positive Guy</h2>
-      <div className={'sm:flex sm:gap-12 sm:justify-between'}>
-        <div className={'max-w-[100px] sm:max-w-[150px] section--image__card'}>
+      <h3 className={'section-indicator pt-4 text-right pb-8'}>Introduce</h3>
+      <div className={'flex gap-4 justify-between'}>
+        <div
+          className={
+            'max-w-[100px] min-w-[100px] sm:max-w-[150px] section--image__card'
+          }
+        >
           <div className={' m-auto section--image__background  '}>
             <Image
               src={introduce}
@@ -17,14 +21,16 @@ export default function Introduce() {
             />
           </div>
         </div>
-        <div className={'max-w-[350px]'}>
+        <div className={'max-w-[350px] space-y-8'}>
           <p>
             I come from Kasinka Mała, currently living in Kraków. I&apos;m
             imaginative person with a strong sense of humor. I put huge emphasis
             on self-development
           </p>
           <p>You will find my resume attached below.</p>
-          <button className={'btn-primary btn-primary__before'}>
+          <button
+            className={'btn-primary btn-primary__before justify-self-end flex'}
+          >
             <a href={'#about'}>Resume</a>
           </button>
         </div>
