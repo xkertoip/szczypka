9d2d88fdf28c4e842cec6644a1fe8acfcb7557f4
Piotr <piotr.szczypka@vp.pl>
2022-11-23 20:29:22 +0100

    optymalization test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <section className={'lg:max-w-[1170px] mx-auto'}>
           <Experience />
         </section>
-        <section
+        {/*        <section
           className={'section-container relative lg:max-w-[1170px] mx-auto'}
         >
           <Skill />
@@ -32,7 +32,7 @@ export default function Home() {
           className={'section-container relative lg:max-w-[1170px] mx-auto'}
         >
           <Work />
-        </section>
+        </section>*/}
 
         <section
           className={
