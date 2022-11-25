74b76b01a6f14048ce65c77ddd6f45b07669c1cc
Piotr <piotr.szczypka@vp.pl>
2022-11-25 13:13:59 +0100

    remove css animations

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,5 +1,6 @@
 import SectionTitle from '../SectionTitle';
 import Link from 'next/link';
+import Perspective3d from '../Perspective3d';
 export default function Summary() {
   return (
     <>
@@ -10,25 +11,26 @@ export default function Summary() {
         indicatorNumber={6}
       />
       <div className={'section-container py-12'}>
-        {/* // perspective*/}
-        <div
-          className={
-            'grid p-8 items-center justify-center drop-shadow-3xl rounded-3xl hover:drop-shadow-4xl bg-zinc-800'
-          }
-        >
-          <p className={'text-2xl pb-8'}>
-            If you are intrested colaboration, just let me know
-          </p>
-
-          <Link
-            href={'/contact'}
+        <Perspective3d width={350} height={300}>
+          <div
             className={
-              'btn-primary btn-primary__before btn-primary__after mx-auto'
+              'grid p-8 items-center justify-center drop-shadow-3xl rounded-3xl hover:drop-shadow-4xl bg-zinc-800'
             }
           >
-            Message
-          </Link>
-        </div>
+            <p className={'text-2xl pb-8'}>
+              If you are intrested colaboration, just let me know
+            </p>
+
+            <Link
+              href={'/contact'}
+              className={
+                'btn-primary btn-primary__before btn-primary__after mx-auto'
+              }
+            >
+              Message
+            </Link>
+          </div>
+        </Perspective3d>
       </div>
     </>
   );
